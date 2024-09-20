335d5162d940bc5cc66c340add9d37144cb9f4d7
YuvanJaswik <arepallyrajavardhanreddy@gmail.com>
2024-09-20 17:10:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-   AWS_ACCESS_KEY_ID     = credentials('AWS_ACCESS_KEY_ID')
-   AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
-} 
+   } 
 
 # VPC
 resource "aws_vpc" "prod-vpc" {
